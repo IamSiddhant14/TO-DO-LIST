dcd94898026f842db940bc67012a83623f1cbde1
Siddhant <siddhantsharma669@gmail.com>
2021-08-22 05:41:20 +0530

    added comments

--- routes/delete.js
+++ routes/delete.js
@@ -1,5 +1,4 @@
 // this will delete the that contact who's is been send by clicking the checkbox
-
 const express = require('express');
 const router = express.Router();
 const deletecontroller = require('../controllers/deletetask_con')
